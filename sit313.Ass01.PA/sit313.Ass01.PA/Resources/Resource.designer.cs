4043bc1cbf5763df03b7a874a58bd438955a3879
randy876224691 <randy876224691@gmail.com>
2016-07-22 13:18:56 +1000

    add icon  image

--- sit313.Ass01.PA/sit313.Ass01.PA/Resources/Resource.designer.cs
+++ sit313.Ass01.PA/sit313.Ass01.PA/Resources/Resource.designer.cs
@@ -41,11 +41,63 @@ namespace sit313.Ass01.PA
 			}
 		}
 		
+		public partial class Drawable
+		{
+			
+			// aapt resource value: 0x7f020000
+			public const int about = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int back = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int bg1 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int detail = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int dollars = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int exit = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int icon = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int insert = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int keyborad = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int left = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int list = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int percent = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int right = 2130837516;
+			
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Drawable()
+			{
+			}
+		}
+		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int myButton = 2131099648;
 			
 			static Id()
 			{
@@ -60,8 +112,8 @@ namespace sit313.Ass01.PA
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f040000
+			public const int Main = 2130968576;
 			
 			static Layout()
 			{
@@ -76,8 +128,8 @@ namespace sit313.Ass01.PA
 		public partial class Mipmap
 		{
 			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			// aapt resource value: 0x7f030000
+			public const int Icon = 2130903040;
 			
 			static Mipmap()
 			{
@@ -92,11 +144,11 @@ namespace sit313.Ass01.PA
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
